215618c217bce768a6986f75f56bd16df932d13a
Niel Geysen <niel.geysen@student.ucll.be>
2026-04-23 21:46:01 +0200

    add site / base paht for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 export default defineConfig({
+  site: "https://nielg.github.io/",
+  base: "CobaltBlueCoaching",
   integrations: [
     icon({
       iconDir: "src/icons",
